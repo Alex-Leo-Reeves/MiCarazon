5a55fb9602c5b79b257e9c27831ddc8d8bbee9fe
Alex-Leo-Reeves <98719040+Alex-Leo-Reeves@users.noreply.github.com>
2024-10-26 04:46:06 +0100

    Add files via upload

---
+++ feelings.js
@@ -0,0 +1,6 @@
+function openLetter() {
+    document.getElementById("letter").style.display = "flex";
+    document.getElementById("letter-text").innerText = 
+        "Yetunde, you were my best friend and my girlfriend, but I messed up. I know my sorry would never be enough because I wasn't there for you when you needed me. Yetunde, I'm here to stay now and I won't leave again. It is not a promise but an assurance; what happened before will not happen again. I miss you so much. All the times I spent with you, every moment we had, every time we told each other 'I love you.' Can I be your forever and will you be Mi Carazon? Open your heart for me. What happened before won't happen again. This is how I really feel. I love you, Khaji.";
+}
+
